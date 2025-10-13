a62ee534ba16823d5bf91d1126bcc26b1091c2ac
Ron <ronshadreck@gmail.com>
2025-10-13 18:50:38 +0200

    Initial Commit

---
+++ Group Investment Tracker/Group_Investment_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_Investment_TrackerApp.swift
+//  Group Investment Tracker
+//
+//  Created by Ron Harifiyati on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Group_Investment_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
